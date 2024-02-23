244890a97015eac0f2b448dfed351821e3527217
Maksym Yarema <macsym2@gmail.com>
2024-02-23 11:38:18 +0200

    init project

---
+++ src/main/java/com/yaremax/berealnotifierbot/BerealNotifierBotApplication.java
@@ -0,0 +1,13 @@
+package com.yaremax.berealnotifierbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerealNotifierBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerealNotifierBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaremax/berealnotifierbot/BerealNotifierBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaremax.berealnotifierbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerealNotifierBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
