6c36826eefa26fcb124715adccc4df772743baf0
Maksym Yarema <macsym2@gmail.com>
2024-02-23 18:53:36 +0200

    using @SneakyThrows

--- src/main/java/com/yaremax/berealnotifierbot/bot/command/DemoTextCommand.java
+++ src/main/java/com/yaremax/berealnotifierbot/bot/command/DemoTextCommand.java
@@ -1,12 +1,12 @@
 package com.yaremax.berealnotifierbot.bot.command;
 
 import com.yaremax.berealnotifierbot.constants.TextCommands;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class DemoTextCommand extends BaseTextCommand {
@@ -18,15 +18,12 @@ public class DemoTextCommand extends BaseTextCommand {
     }
 
     @Override
+    @SneakyThrows
     public void processMessage(AbsSender absSender, Message message, String[] strings) {
         var sendMessage = SendMessage.builder()
                 .text("I'm reply markup response")
                 .chatId(message.getChatId())
                 .build();
-        try {
-            absSender.execute(sendMessage);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
+        absSender.execute(sendMessage);
     }
 }
--- src/main/java/com/yaremax/berealnotifierbot/bot/command/StartCommandHandler.java
+++ src/main/java/com/yaremax/berealnotifierbot/bot/command/StartCommandHandler.java
@@ -1,6 +1,7 @@
 package com.yaremax.berealnotifierbot.bot.command;
 
 import com.yaremax.berealnotifierbot.constants.Actions;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
@@ -10,7 +11,6 @@ import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
@@ -23,29 +23,21 @@ public class StartCommandHandler extends BotCommand {
     }
 
     @Override
+    @SneakyThrows
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-        try {
-            var replyMarkup = InlineKeyboardMarkup.builder()
+        var replyMarkup = InlineKeyboardMarkup.builder()
                 .keyboardRow(List.of(
-                        InlineKeyboardButton.builder()
-                                .text("I'm link to youtube")
-                                .url("https://www.youtube.com")
-                                .build()
-                        ,
                         InlineKeyboardButton.builder()
                                 .text("I'm callback button")
                                 .callbackData(Actions.SOME_ACTION)
                                 .build()
                 ))
                 .build();
-            var sendMessage = SendMessage.builder()
-                    .chatId(chat.getId())
-                    .text("Some text")
-                    .replyMarkup(replyMarkup)
-                    .build();
-            absSender.execute(sendMessage);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
+        var sendMessage = SendMessage.builder()
+                .chatId(chat.getId())
+                .text("Some text")
+                .replyMarkup(replyMarkup)
+                .build();
+        absSender.execute(sendMessage);
     }
 }
